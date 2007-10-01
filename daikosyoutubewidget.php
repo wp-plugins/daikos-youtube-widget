f10aec94355841ce58317f4c879ca8f4d383d706
daiko <daiko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-01 10:02:43 +0000

    tagging version 1.1 Stable version is still version 1.0.8
    git-svn-id: https://plugins.svn.wordpress.org/daikos-youtube-widget/tags/1.1@20571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daikosyoutubewidget.php
+++ daikosyoutubewidget.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Daiko's YouTube Widget
 Plugin URI: http://www.daikos.net/widgets/daikos-youtube-widget/
-Description: Adds a sidebar widget to display random YouTube videos of your own choice.Make your own videolist in the widget-control-panel. Syntax: [YouTube ID]@[Title]<Line Brake>.
+Description: Adds a sidebar widget to display random YouTube videos of your own choice. Make your own videolist in the widget-control-panel and/or add your favorite tag(s), user(s). Syntax: [YouTube ID]@[Title]<Line Brake>.
 Author: Rune Fjellheim
-Version: 1.0.8
+Version: 1.1
 License: GPL
 Author URI: http://www.daikos.net
 */
@@ -23,10 +23,12 @@ function widget_daikos_youtube_init() {
 			$newoptions[$number]['count'] = strip_tags(stripslashes($_POST["daikos-youtube-count-$number"]));
 			$newoptions[$number]['format'] = strip_tags(stripslashes($_POST["daikos-youtube-format-$number"]));
 			$newoptions[$number]['content'] = strip_tags(stripslashes($_POST["daikos-youtube-content-$number"]));
+			$newoptions[$number]['tags'] = strip_tags(stripslashes($_POST["daikos-youtube-tags-$number"]));
+			$newoptions[$number]['users'] = strip_tags(stripslashes($_POST["daikos-youtube-users-$number"]));
 			$newoptions[$number]['show'] = $_POST["daikos-youtube-show-$number"];
 			$newoptions[$number]['slug'] = strip_tags(stripslashes($_POST["daikos-youtube-slug-$number"]));
 		}
-		if ($options[$number]['content']=='') {
+/*		if ($options[$number]['content']=='') {
 			$newoptions[$number]['content'] = 'FBSvtnCr8Wc@Transjoik - Gievrie
 dF0kMBTwDrM@Transjoik - Mustai Amaia
 vkG7psgdl1o@Sámigiella - an Arctic nature language - The Sami language
@@ -38,6 +40,7 @@ zbeEEU_X_B0@Slincraze and Aimen with saami rap
 t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 5eveNk3o1ME@VAJAS - Sparrow of the wind';
 		}
+*/
    		if ($options[$number]['count']=='') {
  			$newoptions[$number]['count'] = 4;
 		}				
@@ -86,25 +89,70 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 		<label for="daikos-youtube-title-<?php echo "$number"; ?>" title="Title above the widget" style="line-height:35px;display:block;">Title: <input type="text" style="width: 442px;" id="daikos-youtube-title-<?php echo "$number"; ?>" name="daikos-youtube-title-<?php echo "$number"; ?>" value="<?php echo htmlspecialchars($options[$number]['title']); ?>" /></label>
 		<label for="daikos-youtube-count-<?php echo "$number"; ?>" title="Number of videos to show" style="line-height:35px;">How many videos to show: <input type="integer" style="width: 80px;" id="daikos-youtube-count-<?php echo "$number"; ?>" name="daikos-youtube-count-<?php echo "$number"; ?>" value="<?php echo htmlspecialchars($options[$number]['count']); ?>" /></label>
 		<label for="daikos-youtube-format-<?php echo "$number"; ?>"  title="Size of the thumbnails." style="line-height:35px;">Format: <select name="daikos-youtube-format-<?php echo "$number"; ?>" id="daikos-youtube-format-<?php echo "$number"; ?>"><option label="Small" value="small" <?php if ($smallSelected){echo "selected";} ?>>Small</option><option label="Medium" value="medium" <?php if ($mediumSelected){echo "selected";} ?>>Medium</option><option label="Large" value="large" <?php if ($largeSelected){echo "selected";} ?>>Large</option></select> </label>
-		<label for="daikos-youtube-content-<?php echo "$number"; ?>" title="Insert your list of videos from YouTube separated by Line Brake. IMPORTANT: No Line Break after the last video!" style="width: 495px; height: 280px;display:block;">Videos [YouTube ID]@[TITLE]<textarea style="width: 470px; height: 240px;" id="daikos-youtube-content-<?php echo "$number"; ?>" name="daikos-youtube-content-<?php echo "$number"; ?>"><?php echo htmlspecialchars($options[$number]['content']); ?></textarea></label>
+		<label for="daikos-youtube-content-<?php echo "$number"; ?>" title="IMPORTANT: No line brake (new line) after the last video!" style="width: 495px; height: 280px;display:block;">Videos [YouTubeID]@[TITLE]{Line brake}<textarea style="width: 470px; height: 240px;" id="daikos-youtube-content-<?php echo "$number"; ?>" name="daikos-youtube-content-<?php echo "$number"; ?>"><?php echo htmlspecialchars($options[$number]['content']); ?></textarea></label>
+		<label for="daikos-youtube-tags-<?php echo "$number"; ?>" title="YouTube tag(s), separated by ," style="line-height:35px;display:block;">Feed from YouTube tag(s), separated by ,  : <input type="text" style="width: 202px;" id="daikos-youtube-tags-<?php echo "$number"; ?>" name="daikos-youtube-tags-<?php echo "$number"; ?>" value="<?php echo htmlspecialchars($options[$number]['tags']); ?>" /></label>
+		<label for="daikos-youtube-users-<?php echo "$number"; ?>" title="YouTube user(s), separated by ," style="line-height:35px;display:block;">Feed from YouTube user(s), separated by ,: <input type="text" style="width: 202px;" id="daikos-youtube-users-<?php echo "$number"; ?>" name="daikos-youtube-users-<?php echo "$number"; ?>" value="<?php echo htmlspecialchars($options[$number]['users']); ?>" /></label>
 		<label for="daikos-youtube-show-<?php echo "$number"; ?>"  title="Show only on specified page(s)/post(s)/category. Default is All" style="line-height:35px;">Display only on: <select name="daikos-youtube-show-<?php echo"$number"; ?>" id="daikos-youtube-show-<?php echo"$number"; ?>"><option label="All" value="all" <?php if ($allSelected){echo "selected";} ?>>All</option><option label="Home" value="home" <?php if ($homeSelected){echo "selected";} ?>>Home</option><option label="Post" value="post" <?php if ($postSelected){echo "selected";} ?>>Post(s)</option><option label="Page" value="page" <?php if ($pageSelected){echo "selected";} ?>>Page(s)</option><option label="Category" value="category" <?php if ($categorySelected){echo "selected";} ?>>Category</option></select></label> 
 		<label for="daikos-youtube-slug-<?php echo "$number"; ?>"  title="Optional limitation to specific page, post or category. Use ID, slug or title." style="line-height:35px;">Slug/Title/ID: <input type="text" style="width: 150px;" id="daikos-youtube-slug-<?php echo "$number"; ?>" name="daikos-youtube-slug-<?php echo "$number"; ?>" value="<?php echo htmlspecialchars($options[$number]['slug']); ?>" /></label>
-		<label for="daikos-youtube-help" title="You can get more help and instructionc on www.daikos.net!" style="line-height:25px;display:block;"><a href="http://www.daikos.net/daikos-youtube-widget/">Help</a></label>
+		<label for="daikos-youtube-help" title="You can get more help and instructionc on www.daikos.net!" style="line-height:25px;display:block;"><a href="http://www.daikos.net/widgets/daikos-youtube-widget/">Help</a></label>
 		<input type="hidden" name="daikos-youtube-submit-<?php echo "$number"; ?>" id="daikos-youtube-submit-<?php echo "$number"; ?>" value="1" />
 	<?php
 	}
 
 	function widget_daikos_youtube($args, $number = 1) {
-		$dytwVersion = "Daiko's YouTube Widget v. 1.0.8";
+		$dytwVersion = "Daiko's YouTube Widget v. 1.1";
 		extract($args);
 		$options = get_option('widget_daikos_youtube');
 		$videoplayeroptions = get_option('widget_daikos_videoplayer');
 		
 		$title = $options[$number]['title'];
+		$tags = str_replace(" ", "", $options[$number]['tags']);
+		$tags = explode(",", $tags);
+		$users = str_replace(" ", "", $options[$number]['users']);
+		$users = explode(",", $users);
 
+  		include_once (ABSPATH . WPINC . '/rss.php');
+		$maxitems = 10;
 
-		if ($options[$number]['content']!='') {
-			$videos = $options[$number]['content'];
+		if (!empty($tags)) {
+			foreach ($tags as $tag) {
+				$feed = fetch_rss("http://www.youtube.com/rss/tag/".$tag.".rss");
+				$items = array_slice($feed->items, 0, $maxitems);				
+				foreach ($items as $item) {
+					$item['link'] = str_replace("http://youtube.com/?v=", "", $item['link']);
+					if ($videos ='') {
+						$videofeed = $videofeed.$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+					else {
+						$videofeed = $videofeed."\n".$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+				} 
+			}
+			
+		}
+		if (!empty($users)) {
+			foreach ($users as $user) {
+				$feed = fetch_rss("http://www.youtube.com/rss/user/".$user."/videos.rss");
+				$items = array_slice($feed->items, 0, $maxitems);
+				foreach ($items as $item) {
+					$item['link'] = str_replace("http://youtube.com/?v=", "", $item['link']);
+					if ($videos ='') {
+						$videofeed = $videofeed.$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+					else {
+						$videofeed = $videofeed."\n".$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+
+				} 
+			}
+
+		}
+		if ($options[$number]['content']!='' ||  !empty($videos)) {
+			$videos = $videos."\n".$options[$number]['content'];
 		}
 		else {
 			$videos = 'FBSvtnCr8Wc@Transjoik - Gievrie
@@ -118,13 +166,21 @@ zbeEEU_X_B0@Slincraze and Aimen with saami rap
 t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 5eveNk3o1ME@VAJAS - Sparrow of the wind';
 		}
-
-		$videos = explode("\n", $videos);										// First we make an array of the videolist
-		$count = $options[$number]['count'];
+		$videos = explode("\n", $videos);										// First we make an array of the videolist 
+		foreach($videos as $key => $value) {                                    // Take out empty videos
+			if (strlen($value) < 10) $value = "";
+			if($value == "" || $value == " " || is_null($value)) {
+				unset($videos[$key]);
+			}
+		}                               
+		$videos = array_unique($videos);                                        // Take out duplicates
+		$videos = array_values($videos);                                        // Reorder the array
+		$count = $options[$number]['count'];                                    // Count the videos available
 		$countvideos = count($videos);
-		if ($count>$countvideos){$count=$countvideos;}							// Prevent user from trying to show more videos than is available
-		$random = array_rand($videos, $count); 
-		for ($num=0; $num<$count; ++$num)  {
+		
+		if ($count>$countvideos){$count=$countvideos;}							// Prevent user from trying to show more videos than are available
+		$random = array_rand($videos, $count);                                  // Pick a random selection of videos
+		for ($num=0; $num<$count; ++$num)  {                                    // Go through the list and establish the mediaID and title arrays
             $video[$num] = $videos[$random[$num]];
 			$temp = explode("@", $video[$num]);
 			$mediaID[$num] = $temp[0];
@@ -132,11 +188,11 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
         }
 		$show = $options[$number]['show'];                                      // Get the setting on where to show the widget
 		$slug = $options[$number]['slug'];                                      // Optional Slug/Title/PageID on where to show it
-		$showvideoplayer = $videoplayeroptions['show'];
+		$showvideoplayer = $videoplayeroptions['show'];                         // Check the VideoPlayer Show and Slug
 		$slugvideoplayer = $videoplayeroptions['slug'];
 		$showplayer = false;
 		
-   		switch ($showvideoplayer) {
+   		switch ($showvideoplayer) {                                             // Determine whether to use ThickBox or Daiko's VideoPlayer
 				case "all": 
 					$showplayer = true;
 					break;
@@ -163,13 +219,13 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 								
 			}		
 		
-		$format = $options[$number]['format'];
+		$format = $options[$number]['format'];                                  // What format to show the thumbnail images in
 		if (is_active_widget('widget_daikos_videoplayer') && $showplayer) {
-			$width = $videoplayeroptions['width'];
+			$width = $videoplayeroptions['width'];                              // Format to play the video in
 			if (empty($width)) $width = 200;
 			$heigth = round($width*0.82352941);
 			for ($i=0; $i<$count ;++$i) {
-				$fulltext = $fulltext.'<div class="DYTWWrapperOuter'.$format.'"><div class="DYTWWrapperInner'.$format.'"><a onclick="var so = new SWFObject(\'http://www.youtube.com/v/'.$mediaID[$i].'&amp;autoplay=1\', \'DYTW'.$i.'\', \''.$width.'\', \''.$heigth.'\', \'8\', \'#336699\');so.addParam(\'wmode\', \'transparent\');'.$addParameter.' so.write(\'BigPlayer2\');" href="#"><img src="http://img.youtube.com/vi/'.$mediaID[$i].'/default.jpg" alt="'.$videotitle[$i].'" title="'.$videotitle[$i].'"><div class="DYTWIcon'.$format.'"><img src="'.get_bloginfo("url").'/wp-content/plugins/daikos-youtube-widget/play.png" alt="play" ></div></a></div></div>'; 
+				$fulltext = $fulltext.'<div class="DYTWWrapperOuter'.$format.'"><div class="DYTWWrapperInner'.$format.'"><a onclick="var so = new SWFObject(\'http://www.youtube.com/v/'.$mediaID[$i].'&amp;autoplay=1\', \'DYTW'.$i.'\', \''.$width.'\', \''.$heigth.'\', \'8\', \'#336699\');so.addParam(\'wmode\', \'transparent\');'.$addParameter.' so.write(\'BigPlayer2\');" href="#BigPlayer2"><img src="http://img.youtube.com/vi/'.$mediaID[$i].'/default.jpg" alt="'.$videotitle[$i].'" title="'.$videotitle[$i].'"><div class="DYTWIcon'.$format.'"><img src="'.get_bloginfo("url").'/wp-content/plugins/daikos-youtube-widget/play.png" alt="play" title="play" ></div></a></div></div>'; 
 			}
 			$fulltext = '<div class="DYTWContainer">'.$fulltext.'<div class="DYTWcredits"><a href="http://www.daikos.net" title="'.$dytwVersion.'">YouTube Widget by Daiko</a></div></div>';
 		} 
@@ -177,7 +233,7 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 			$width = 500;
 			$heigth = round($width*0.82352941);
 			for ($i=0; $i<$count ;++$i) {
-				$fulltext = $fulltext.'<div class="DYTWWrapperOuter'.$format.'"><div class="DYTWWrapperInner'.$format.'"><a onclick="var so = new SWFObject(\'http://www.youtube.com/v/'.$mediaID[$i].'&amp;autoplay=1\', \'DYTW'.$i.'\', \'500\', \'375\', \'8\', \'#336699\');so.addParam(\'wmode\', \'transparent\');'.$addParameter.' so.write(\'flashcontent\');" href="#TB_inline?height=385&amp;width=500&amp;inlineId=flashcontent" class="thickbox"><img src="http://img.youtube.com/vi/'.$mediaID[$i].'/default.jpg" alt="'.$videotitle[$i].'" title="'.$videotitle[$i].'"><div class="DYTWIcon'.$format.'"><img src="'.get_bloginfo("url").'/wp-content/plugins/daikos-youtube-widget/play.png" alt="play" ></div></a></div></div>'; 
+				$fulltext = $fulltext.'<div class="DYTWWrapperOuter'.$format.'"><div class="DYTWWrapperInner'.$format.'"><a onclick="var so = new SWFObject(\'http://www.youtube.com/v/'.$mediaID[$i].'&amp;autoplay=1\', \'DYTW'.$i.'\', \'500\', \'375\', \'8\', \'#336699\');so.addParam(\'wmode\', \'transparent\');'.$addParameter.' so.write(\'flashcontent\');" href="#TB_inline?height=385&amp;width=500&amp;inlineId=flashcontent" class="thickbox"><img src="http://img.youtube.com/vi/'.$mediaID[$i].'/default.jpg" alt="'.$videotitle[$i].'" title="'.$videotitle[$i].'"><div class="DYTWIcon'.$format.'"><img src="'.get_bloginfo("url").'/wp-content/plugins/daikos-youtube-widget/play.png" alt="play" title="play" ></div></a></div></div>'; 
 			}
 			$fulltext = '<div class="DYTWContainer">'.$fulltext.'<div class="DYTWcredits"><a href="http://www.daikos.net" title="'.$dytwVersion.'">YouTube Widget by Daiko</a></div></div>';
 		}
@@ -304,7 +360,7 @@ Loading....
 		for ($i = 1; $i <= 9; $i++) {
 			$name = array('Daiko\'s YouTube Widget %s', 'widgets', $i);
 			register_sidebar_widget($name, $i <= $number ? 'widget_daikos_youtube' : /* unregister */ '', $i);
-			register_widget_control($name, $i <= $number ? 'widget_daikos_youtube_control' : /* unregister */ '', 490, 455, $i);
+			register_widget_control($name, $i <= $number ? 'widget_daikos_youtube_control' : /* unregister */ '', 490, 495, $i);
 		}
 		add_action('sidebar_admin_setup', 'widget_daikos_youtube_setup');
 		add_action('sidebar_admin_page', 'widget_daikos_youtube_page');
@@ -380,15 +436,58 @@ function widget_daikos_videoplayer_init() {
 		$youtubeoptions = get_option('widget_daikos_youtube');
         $title = $options['title'];
         $width = $options['width'];
-		$show = $options['show'];                                      // Get the setting on where to show the widget
-		$slug = $options['slug'];                                      // Optional Slug/Title/PageID on where to show it
+		$show = $options['show'];                                      			// Get the setting on where to show the widget
+		$slug = $options['slug'];                                      			// Optional Slug/Title/PageID on where to show it
 		if (empty($width)) $width = 200;
 		$heigth = round($width*0.82352941);
         $number = 1;
+		$tags = str_replace(" ", "", $youtubeoptions[$number]['tags']); 		
+		$tags = explode(",", $tags);
+		$users = str_replace(" ", "", $youtubeoptions[$number]['users']);
+		$users = explode(",", $users);
 
-		if ($youtubeoptions[$number]['content']!='') {
-			$videos = $youtubeoptions[$number]['content'];
+  		include_once (ABSPATH . WPINC . '/rss.php');
+		$maxitems = 10;
+
+		if (!empty($tags)) {
+			foreach ($tags as $tag) {
+				$feed = fetch_rss("http://www.youtube.com/rss/tag/".$tag.".rss");
+				$items = array_slice($feed->items, 0, $maxitems);
+				foreach ($items as $item) {
+					$item['link'] = str_replace("http://youtube.com/?v=", "", $item['link']);
+					if ($videos ='') {
+						$videofeed = $videofeed.$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+					else {
+						$videofeed = $videofeed."\n".$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+				} 
 			}
+		}
+
+		if (!empty($users)) {
+			foreach ($users as $user) {
+				$feed = fetch_rss("http://www.youtube.com/rss/user/".$user."/videos.rss");
+				$items = array_slice($feed->items, 0, $maxitems);
+				foreach ($items as $item) {
+					$item['link'] = str_replace("http://youtube.com/?v=", "", $item['link']);
+					if ($videos ='') {
+						$videofeed = $videofeed.$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+					else {
+						$videofeed = $videofeed."\n".$item['link']."@".$item['title'];
+						$videos = $videos.$videofeed;
+					}
+                } 
+			}
+		}
+
+		if ($youtubeoptions[$number]['content']!='' ||  !empty($videos)) {
+			$videos = $videos.$youtubeoptions[$number]['content'];
+		}
 		else {
 			$videos = 'FBSvtnCr8Wc@Transjoik - Gievrie
 dF0kMBTwDrM@Transjoik - Mustai Amaia
@@ -403,6 +502,14 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 		}
 
 		$videos = explode("\n", $videos);										// First we make an array of the videolist
+		foreach($videos as $key => $value) {                                    // Take out empty videos
+			if (strlen($value) < 10) $value = "";
+			if($value == "" || $value == " " || is_null($value)) {
+				unset($videos[$key]);
+			}
+		}                               
+		$videos = array_unique($videos);                                        // Take out duplicates
+		$videos = array_values($videos);                                        // Reorder the array
 		$video = wptexturize( $videos[ mt_rand(0, count($videos) - 1 ) ] );
 		$temp = explode("@", $video);
 		$mediaID = $temp[0];
--- readme.txt
+++ readme.txt
@@ -10,6 +10,8 @@ Adds a sidebar widget to display random YouTube videos of your own choice. Uses 
 
 Adds a sidebar widget to display a defined number of random YouTube videos of your own choice. Make your own videolist in the widget-control-panel. Syntax: {YouTubeID}@{Title}(Line Brake) without the brackets. Do not add a (Line Brake) after the last video in the list.
 
+The widget also supports RSS-feeds from tag(s) and user(s). NEW in version 1.1.
+
 The widget includes a plugin to activate ThickBox 3.1 developed by Cody Lindley to display videos as a pop-up and uses swfObjects 1.5 by Geoff Stearns. If you by any chance don't want to use the ThickBox player activate the included Daiko's VideoPlayer Widget. The two player options can be mixed. If both are active on a page, Daiko's VideoPlayer Widget will override the ThickBox pop-up player.
 
 If you want to use ThickBox and swfObjects on other parts of your blog, the libraries are available as long as Daiko's YouTube Widget and Daiko's ThickBox plugin are activated.
@@ -20,7 +22,7 @@ A core part of ThickBox is the jquery.js library. I've found some problems with 
 
 Author: Rune Fjellheim
 
-Version: 1.0.8
+Version: 1.1
 
 Author URI: http://www.daikos.net
 
@@ -44,6 +46,10 @@ your videos with the following syntax:
 {YouTubeID}@{Title}(Line Brake)
 
 Without the brackets.
+
+Add comma separated tag(s) and/or user(s) to add rss feeds to your selection of videos from where the displayed videos will be selected.
+
+Daiko's VideoPlayer selects its random display video from the specified videos in the 1st Daiko's YouTube Widget (Daiko's YouTube Widget 1).
 
 IMPORTANT! Do not add a (Line brake) on the last line...
 
