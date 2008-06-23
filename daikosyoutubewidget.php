3279961e4b7be3e4ec688e2af68801959c0bdbf5
daiko <daiko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-23 14:48:01 +0000

    9th version of internationalization
    git-svn-id: https://plugins.svn.wordpress.org/daikos-youtube-widget/trunk@51530 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daikosyoutubewidget.php
+++ daikosyoutubewidget.php
@@ -262,7 +262,7 @@ t_xQN6s_COw@Sofia ja Anna - Du Calmmit (your eyes)
 			$fulltext = '<div class="DYTWContainer">'.$fulltext.'<div class="DYTWcredits"><a href="http://www.daikos.net" title="'.$dytwVersion.'">YouTube Widget by Daiko</a></div></div>';
 		}
 		else {
-		   	$fulltext = echo __("This widget requires that you activate Daiko's ThickBox Plugin or place the Daiko's VideoPlayer Widget."); 
+		   	$fulltext = echo __("Activate Daiko's ThickBox Plugin or place Daiko's VideoPlayer Widget!"); 
 		}
 		
 /* And do the widget dance! */
